56e20ed84532615383828c0a6b6d557525df70b0
Juan <juanndiiego2@gmail.com>
2024-11-18 22:35:06 -0500

    Se Crea estructura basica del login

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { HomepageComponent } from './features/homepage/homepage.component';
+import { LoginComponent } from './features/auth/login/login.component';
+import { RegisterComponent } from './features/auth/register/register.component';
+import { InventoryComponent } from './features/stock/inventory/inventory.component';
+import { AddProductComponent } from './features/stock/add-product/add-product.component';
+import { HeaderComponent } from './features/core/header/header.component';
+import { SidebarComponent } from './features/core/sidebar/sidebar.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomepageComponent},
+  { path: 'homepage', component: HomepageComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'register', component: RegisterComponent},
+  { path: 'inventory', component: InventoryComponent},
+  { path: 'addProduct', component: AddProductComponent},
+  { path: 'header', component: HeaderComponent},
+  { path: 'sidebar', component: SidebarComponent },
+  { path: '**', redirectTo: '' },
+];
